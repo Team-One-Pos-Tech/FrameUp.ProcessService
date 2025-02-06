91adcaa8156acae2fc1a8240b2d18b1e870f344f
Icaro Sousa <icaro.py@gmail.com>
2025-02-06 00:29:43 +0000

    feat: adds event notification when processing order requests;

---
+++ src/FrameUp.ProcessService.Application/Models/Events/UpdateOrderStatusEvent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using FrameUp.ProcessService.Domain.Enums;
+using MassTransit;
+
+namespace FrameUp.ProcessService.Application.Models.Events;
+
+[MessageUrn("frameup-order-service")]
+[EntityName("update-order-status")]
+public record UpdateOrderStatusEvent(Guid OrderId, ProcessingStatus Status);
